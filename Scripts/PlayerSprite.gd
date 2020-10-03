761014c3c4a821866697ea9cd7eef3346fcb9fa1
Justinas Grigas <jgrigas@elam.co.uk>
2020-10-03 11:35:12 +0100

    Implemented moving platforms.

--- Scripts/PlayerSprite.gd
+++ Scripts/PlayerSprite.gd
@@ -1,7 +1,21 @@
 extends AnimatedSprite
 
-func flip_sprite(x_velocity: float):
+var _threshold = 0.01
+
+func _flip_sprite(x_velocity: float):
 	if x_velocity == 0:
 		return
 	flip_h = x_velocity < 0
+	return
+
+func animate(velocity: Vector2):
+	_flip_sprite(velocity.x)
+	# Jumping.
+	if abs(velocity.y) > _threshold:
+		animation = "Jump"
+	# Idle.
+	elif abs(velocity.x) <= _threshold:
+		animation = "Idle"
+	else:
+		animation = "Run"
 	return
